e1d4d4c3e52c4f5cffd0e511963b327a5b18a875
Orion-wyc <852722340@qq.com>
2018-05-26 21:50:41 +0800

    Update
    ver 1.0

---
+++ src/finalHomework/StartGame.java
@@ -0,0 +1,15 @@
+package finalHomework;
+
+/*Orion233
+ * 2018-05-25
+ * CS1604 JNU
+ * Wang Yuchen 
+ * 
+ * */
+import java.io.IOException;
+
+public class StartGame {
+	public static void main(String args[]) throws IOException {
+		new BeginningDialog();
+	}
+}
